fd96448537a0cec6f0195c12e164462f76867317
Wonteak <lwt1849@gmail.com>
2017-12-29 14:31:19 +0900

    Initial version

--- src/PID.cpp
+++ src/PID.cpp
@@ -14,10 +14,12 @@ void PID::Init(double Kp, double Ki, double Kd) {
     Kp_ = Kp;
     Ki_ = Ki;
     Kd_ = Kd;
+
+    cmd_ = 0.0;
 }
 
 void PID::UpdateError(double cte) {
-    d_error_ = p_error_ - cte;
+    d_error_ = cte - p_error_;
     p_error_ = cte;
     i_error_ += cte;
 
